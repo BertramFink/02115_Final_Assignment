1024c707d4acf645e4806cf1406a2b3a5e6ec48c
Sebastian <sebastian@familienholdt.dk>
2024-11-18 18:05:49 +0100

    added register file and functions to manipulate memory and register file

--- simulator.c
+++ simulator.c
@@ -3,13 +3,44 @@
 #include <inttypes.h>
 
 struct Memory {
-  int length;
+  size_t length;
   uint8_t buffer[];
 };
 
-struct Memory *instructionMemory;
+struct RegisterFile {
+  int PC;
+  int GP[32];
+};
 
-void initInstructionMemory(char *fileName) {
+struct Memory *iMem;
+
+struct RegisterFile *rf;
+
+int memRead(struct Memory *memory, size_t index) {
+  if (index < memory->length) {
+    return * (int *) &memory->buffer[index];
+  }
+}
+
+void memWrite(struct Memory *memory, size_t index, int value) {
+  if (index < memory->length) {
+    * (int *) &memory->buffer[index] = value;
+  }
+}
+
+int regRead(size_t index) {
+  if (index < 32) {
+    return rf->GP[index];
+  }
+}
+
+void regWrite(size_t index, int value) {
+  if (index < 32) {
+    rf->GP[index] = value;
+  }
+}
+
+void initIMem(char *fileName) {
   FILE *file = fopen(fileName, "rb");
   if (file == NULL) {
     perror("Error opening file");
@@ -18,12 +49,23 @@ void initInstructionMemory(char *fileName) {
   fseek(file, 0, SEEK_END);
   int length = ftell(file);
   fseek(file, 0, SEEK_SET);
-  instructionMemory = (struct Memory*) malloc(sizeof(struct Memory) + length);
-  instructionMemory->length = length;
-  int i = fread(instructionMemory->buffer, 1, length, file);
+  iMem = (struct Memory*) malloc(sizeof(struct Memory) + length);
+  iMem->length = length;
+  int i = fread(iMem->buffer, 1, length, file);
   if (i != length) {
     perror("Error reading file");
     exit(1);
+  }
+}
+
+void printRegisterFile() {
+  printf("\nRegister Content:\n PC=0x%08x\n", rf->PC);
+  for (int i = 0; i < 4; i ++) {
+    for (int j = 0; j < 16; j += 4) {
+      int n = i + j;
+      printf("%sx%d=0x%08x   ", n < 10 ? " " : "", n, regRead(n));
+    }
+    printf("\n");
   }
 }
 
@@ -33,11 +75,14 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  initInstructionMemory(argv[1]);
+  initIMem(argv[1]);
+  rf = (struct RegisterFile *) malloc(sizeof(struct RegisterFile));
 
-  for (int i = 0; i < instructionMemory->length; i ++) {
-    printf("%4d: 0x%08x\n", i, instructionMemory->buffer[i]);
+  for (rf->PC = 0; rf->PC < iMem->length; rf->PC += 4) {
+    printf("%4d: 0x%08x\n", rf->PC, memRead(iMem, rf->PC));
   }
+
+  printRegisterFile();
 
   return 0;
 }
